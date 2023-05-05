5a89f6a10f7798736553332134c1a43bde8c51a2
Vaibhav Rajani <vaibhav.rajanivit@gmail.com>
2023-05-05 18:46:55 -0400

    Initial Commit

---
+++ MyStock/MyStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStockApp.swift
+//  MyStock
+//
+//  Created by Vaibhav on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MyStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
